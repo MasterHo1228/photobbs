cd3aad78b6c302332af6ba44b8235df7353302b1
MasterHo1228 <masterho1228@gmail.com>
2021-11-17 15:14:08 +0800

    Split header and footer layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,17 +8,10 @@
   <title>@yield('title','Photo BBS') - 灰常低调的摄影生活杂志论坛</title>
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="/">Photo BBS</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-        <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-      </ul>
-    </div>
-  </nav>
+  @include('layouts._header')
   <div class="container">
     @yield('content')
+    @include('layouts._footer')
   </div>
 </body>
 </html>
