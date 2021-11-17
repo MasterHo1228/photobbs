96c5cebbb89890eed498f2f0c8119d04581d6c6e
MasterHo1228 <masterho1228@gmail.com>
2021-11-17 14:07:24 +0800

    Initialize style.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,9 +4,21 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/app.css">
   <title>@yield('title','Photo BBS') - 灰常低调的摄影生活杂志论坛</title>
 </head>
 <body>
-  @yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Photo BBS</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    @yield('content')
+  </div>
 </body>
 </html>
