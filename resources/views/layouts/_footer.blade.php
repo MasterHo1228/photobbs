221c8161090fc25bbb12fe669b127ac0118326c7
MasterHo1228 <masterho1228@gmail.com>
2021-11-17 15:47:40 +0800

    Add name route.

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
   </a>
 
   <div class="float-right">
-    <a href="/about" >关于</a>
+    <a href="{{ route('about') }}" >关于</a>
   </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/">Photo BBS</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Photo BBS</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+      <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
   </div>
