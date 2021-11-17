959917264e090d7d47e0ed2250398d2518262216
MasterHo1228 <masterho1228@gmail.com>
2021-11-17 10:56:28 +0800

    Get some static layout.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
