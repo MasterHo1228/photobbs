80cadf0d4de4ca496426cf47b5f9b32c133b90cb
MasterHo1228 <masterho1228@gmail.com>
2021-11-18 17:14:20 +0800

    Add auto-login after user registering

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <script src="{{ mix('js/app.js') }}"></script>
   <title>@yield('title','Photo BBS') - 灰常低调的摄影生活杂志论坛</title>
 </head>
 <body>
