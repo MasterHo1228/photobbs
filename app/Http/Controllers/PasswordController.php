83daadd98685f3581101b28d52e5d0560badb34e
MasterHo1228 <masterho1228@gmail.com>
2021-11-20 19:33:38 +0800

    Build article model

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -14,8 +14,8 @@ class PasswordController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('throttle:2,1', [
-            'only' => ['showLinkRequestForm']
+        $this->middleware('throttle:3,10', [
+            'only' => ['sendResetLinkEmail']
         ]);
     }
     public function showLinkRequestForm()
--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,8 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    public function articles(){
+        return $this->hasMany(Article::class);
+    }
 }
