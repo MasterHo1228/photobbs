0352aff4624ebf5106308a0c139ab9da3068cb35
MasterHo1228 <masterho1228@gmail.com>
2021-11-21 16:33:29 +0800

    优化:用户关注列表&粉丝列表 增加显示关注时间

--- resources/views/users/show_follows.blade.php
+++ resources/views/users/show_follows.blade.php
@@ -5,17 +5,17 @@
 <div class="offset-md-2 col-md-8">
   <h2 class="mb-4 text-center">{{ $title }}</h2>
 
-  <div class="list-group list-group-flush">
+  <ul class="list-group list-group-flush">
     @foreach ($users as $user)
-      <div class="list-group-item">
+      <li class="list-group-item">
         <img class="mr-3" src="{{ $user->gravatar() }}" alt="{{ $user->name }}" width=32>
         <a href="{{ route('users.show', $user) }}">
           {{ $user->name }}
         </a>
-      </div>
-
+        <span class="follow-time badge"> 关注时间：{{ $user->created_at->diffForHumans() }} </span>
+      </li>
     @endforeach
-  </div>
+  </ul>
 
   <div class="mt-3">
     {!! $users->render() !!}
