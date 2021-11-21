35234bd6c1c5d7b0d57ec967c558e48a5687ade6
MasterHo1228 <masterho1228@gmail.com>
2021-11-21 14:45:31 +0800

    优化：修改 用户登录账号后 跳转回到网站首页

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -32,8 +32,7 @@ class SessionsController extends Controller
        if (Auth::attempt($credentials,$request->has('remember'))) {
             if (Auth::user()->activated){
                 session()->flash('success', '欢迎回来！');
-                $fallback = route('users.show', Auth::user());
-                return redirect()->intended($fallback);
+                return redirect('/');
             } else {
                 Auth::logout();
                 session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
