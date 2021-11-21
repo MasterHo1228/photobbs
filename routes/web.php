de4bfe1da27387edbb5261d7dceac9ab89e7ba27
MasterHo1228 <masterho1228@gmail.com>
2021-11-21 15:05:11 +0800

    Add user following list

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Static Pages*/
+//Static Pages
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
-/*Users*/
+//Users
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
-/*Sessions*/
+//Sessions
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
+//Password Resets
 Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
 Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
 
 Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 
+//Articles
 Route::resource('articles', 'ArticlesController', ['only' => ['store', 'destroy']]);
+
+//Following
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
