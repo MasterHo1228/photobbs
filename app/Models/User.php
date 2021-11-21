0ef57b18ea0e2716670b43fc29ab6ac605723a3f
MasterHo1228 <masterho1228@gmail.com>
2021-11-21 13:58:35 +0800

    Build user following relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,38 @@ class User extends Authenticatable
     public function feed(){
         return $this->articles()->orderBy('created_at', 'desc');
     }
+
+    //粉丝关系
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    //关注名单
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
+    //关注动作
+    public function follow($user_ids){
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    //取消关注动作
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
 }
