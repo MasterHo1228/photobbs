160a490c5713d2688e0e86c000d69f1ef8ebe4f7
MasterHo1228 <masterho1228@gmail.com>
2021-11-27 18:21:17 +0800

    Show user avatar

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,10 +8,10 @@
 
   <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
     <div class="card ">
-      <img class="card-img-top" src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
+      <img class="card-img-top" src="{{ $user->avatar }}" alt="{{ $user->name }}">
       <div class="card-body">
             <h5><strong>个人简介</strong></h5>
-            <p>{{ $user->introduction ?? '这个人很懒，没有写简介~' }}</p>
+            <p>{{ $user->introduction ?? '(这个人很懒，没有写简介~)' }}</p>
             <hr>
             <h5><strong>注册于</strong></h5>
             <p>{{ $user->created_at->diffForHumans() }}</p>
