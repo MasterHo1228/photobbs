d603792a0fcd98269b55001ee4273d8b531b58f2
MasterHo1228 <masterho1228@gmail.com>
2021-11-28 15:26:26 +0800

    Made topic seeder.

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 生成数据集合
+        User::factory()->count(10)->create();
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'JonyHo';
+        $user->email = 'jonyho@test.io';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
