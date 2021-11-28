0b577ee096c11bc97fa8c15749a75b5e32ba63d1
MasterHo1228 <masterho1228@gmail.com>
2021-11-28 14:28:12 +0800

    Build post category model

---
+++ database/migrations/2021_11_28_141844_seed_categories_data.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name'        => '作品分享',
+                'description' => '分享创造，分享发现',
+            ],
+            [
+                'name'        => '日常分享',
+                'description' => '分享一些日常生活中好看的，好玩的...',
+            ],
+            [
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
+            ],
+            [
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('categories')->truncate();
+    }
+}
