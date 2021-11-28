70ce68c34a0d3d3505d7efea86f3b12c50e40779
MasterHo1228 <masterho1228@gmail.com>
2021-11-28 18:25:09 +0800

    Topic sorting

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,11 +8,12 @@ use App\Models\Topic;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
-        // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = Topic::with('user', 'category')->where('category_id', $category->id)->paginate(20);
-        // 传参变量话题和分类到模板中
+        $topics = $topic->withOrder($request->order)
+                        ->where('category_id', $category->id)
+                        ->with('user', 'category')   // 预加载防止 N+1 问题
+                        ->paginate(20);
         return view('topics.index', compact('topics', 'category'));
     }
 }
