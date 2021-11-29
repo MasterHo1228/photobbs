3d153a608dcc058b9af00eda82f4426275e1f83e
MasterHo1228 <masterho1228@gmail.com>
2021-11-29 15:53:14 +0800

    Users can post topic now.

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class TopicsController extends Controller
 {
@@ -29,19 +31,24 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic' ,'categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request,Topic $topic)
 	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		$topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+		return redirect()->route('topics.show', $topic->id)->with('success', '话题发布成功！');
 	}
 
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
@@ -49,7 +56,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '话题编辑成功！');
 	}
 
 	public function destroy(Topic $topic)
