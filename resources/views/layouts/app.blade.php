caaa01c8d3696d3d57ca86bda95ef9b53199425a
MasterHo1228 <masterho1228@gmail.com>
2021-12-01 15:12:35 +0800

    Build show topic page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="@yield('description', '灰常低调的摄影生活杂志论坛')" />
 
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
