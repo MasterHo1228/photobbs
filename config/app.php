b6a813890bf9270c90ceab5e20d421a309c23b94
MasterHo1228 <masterho1228@gmail.com>
2021-12-01 16:15:41 +0800

    Fix XSS filling leak

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Mews\Captcha\CaptchaServiceProvider::class,
+        Mews\Purifier\PurifierServiceProvider::class,
     ],
 
     /*
@@ -232,6 +233,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Captcha' => Mews\Captcha\Facades\Captcha::class,
+        'Purifier' => Mews\Purifier\Facades\Purifier::class,
 
     ],
 
