0a1db26abe375ef78cc04fd14b1aa558965128c4
MasterHo1228 <masterho1228@gmail.com>
2021-12-02 15:13:49 +0800

    优化-TranslateTopicSlug:标题带非英文的时候才调用翻译API

--- app/Jobs/TranslateTopicSlug.php
+++ app/Jobs/TranslateTopicSlug.php
@@ -27,8 +27,14 @@ class TranslateTopicSlug implements ShouldQueue
 
     public function handle()
     {
-        // 请求翻译 API 接口进行翻译，翻译后生成slug链接后缀
-        $slug = app(SlugHandler::class)->generate_with_translate($this->topic->title);
+        $title = $this->topic->title;
+        if (!ctype_alpha($title)) {
+            //非英文 请求翻译 API 接口进行翻译，翻译后生成slug链接后缀
+            $slug = app(SlugHandler::class)->generate_with_translate($title);
+        } else {
+            //否则字符直接转义
+            $slug = app(SlugHandler::class)->generate($title);
+        }
 
         // 为了避免模型监控器死循环调用，我们使用 DB 类直接对数据库进行操作
         DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
