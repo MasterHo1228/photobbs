04d8f53aa5973a218329676818ef98dd7be6b5ed
MasterHo1228 <masterho1228@gmail.com>
2021-12-06 10:29:43 +0800

    Add $cache_key

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -12,6 +12,9 @@ class Link extends Model
 
     protected $fillable = ['title', 'link'];
 
+    public $cache_key = 'photobbs_links';
+    protected $cache_expire_in_seconds = 1440 * 60;
+
     public function getAllCached(){
         // 尝试从缓存中取出 cache_key 对应的数据。如果能取到，便直接返回数据。
         // 否则运行匿名函数中的代码来取出 links 表中所有的数据，返回的同时做了缓存。
