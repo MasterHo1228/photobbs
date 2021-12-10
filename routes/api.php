3a2a6aa426441af49e0382eb71bb9fbce30c995d
MasterHo1228 <masterho1228@gmail.com>
2021-12-10 20:05:35 +0800

    Fixup

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')
     ->namespace('Api')
     ->name('api.v1.')
-    ->middleware('throttle:1,1')
     ->group(function() {
         // 短信验证码
         Route::post('verificationCodes', 'VerificationCodesController@store')
@@ -29,6 +28,11 @@ Route::prefix('v1')
         // 用户注册
         Route::post('users', 'UsersController@store')
         ->name('users.store');
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function () {
+
+            });
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
