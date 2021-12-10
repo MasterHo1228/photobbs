99557ba3b84ea672e9d24e37dcf06d439d05c461
MasterHo1228 <masterho1228@gmail.com>
2021-12-10 17:06:12 +0800

    Linit register request

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', 'UsersController@store')
-    ->name('users.store');
+Route::prefix('v1')
+    ->namespace('Api')
+    ->name('api.v1.')
+    ->middleware('throttle:1,1')
+    ->group(function() {
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')
+            ->name('verificationCodes.store');
+        // 用户注册
+        Route::post('users', 'UsersController@store')
+        ->name('users.store');
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
