0a4c89bb709f55b465e75e7414c5e3a0ba811275
MasterHo1228 <masterho1228@gmail.com>
2021-12-22 17:06:36 +0800

    Show categories list api

--- app/Http/Controllers/Api/ImagesController.php
+++ app/Http/Controllers/Api/ImagesController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Image;
 use App\Http\Requests\Api\ImageRequest;
--- routes/api.php
+++ routes/api.php
@@ -36,22 +36,6 @@ Route::prefix('v1')
                     ->name('captchas.store');
             });
 
-        // 用户注册
-        Route::post('users', 'UsersController@store')
-            ->name('users.store');
-        // 第三方登录
-        Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-            ->where('social_type', 'wechat')->name('socials.authorizations.store');
-        // 登录
-        Route::post('authorizations', 'AuthorizationsController@store')
-            ->name('authorizations.store');
-        // 刷新token
-        Route::put('authorizations/current', 'AuthorizationsController@update')
-            ->name('authorizations.update');
-        // 删除token
-        Route::delete('authorizations/current', 'AuthorizationsController@destroy')
-            ->name('authorizations.destroy');
-
         Route::middleware('throttle:' . config('api.rate_limits.access'))
             ->group(function () {
                 // 游客可以访问的接口
@@ -71,7 +55,27 @@ Route::prefix('v1')
                     Route::post('images', 'ImagesController@store')
                         ->name('images.store');
                 });
-            });
+        });
+
+        // 用户注册
+        Route::post('users', 'UsersController@store')
+            ->name('users.store');
+        // 第三方登录
+        Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->where('social_type', 'wechat')->name('socials.authorizations.store');
+        // 登录
+        Route::post('authorizations', 'AuthorizationsController@store')
+            ->name('authorizations.store');
+        // 刷新token
+        Route::put('authorizations/current', 'AuthorizationsController@update')
+            ->name('authorizations.update');
+        // 删除token
+        Route::delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('authorizations.destroy');
+
+        // 分类列表
+        Route::get('categories', 'CategoriesController@index')
+        ->name('categories.index');
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
