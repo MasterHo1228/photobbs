c7478ef94051b2c6efb40904528576ac0703cc9c
MasterHo1228 <masterho1228@gmail.com>
2021-12-22 12:23:42 +0800

    Get user info api

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,21 @@ Route::prefix('v1')
         // 删除token
         Route::delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('authorizations.destroy');
+
+        Route::middleware('throttle:' . config('api.rate_limits.access'))
+            ->group(function () {
+                // 游客可以访问的接口
+                // 某个用户的详情
+                Route::get('users/{user}', 'UsersController@show')
+                    ->name('users.show');
+
+                // 登录后可以访问的接口
+                Route::middleware('auth:api')->group(function() {
+                    // 当前登录用户信息
+                    Route::get('user', 'UsersController@me')
+                        ->name('user.show');
+                });
+            });
 });
 
 Route::prefix('v2')->name('api.v2.')->group(function() {
