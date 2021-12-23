0613414bcbe66a5c55d21e6fbe3cfdb04c821eb6
MasterHo1228 <masterho1228@gmail.com>
2021-12-23 19:38:56 +0800

    Post reply api

--- routes/api.php
+++ routes/api.php
@@ -69,6 +69,10 @@ Route::prefix('v1')
                     Route::resource('topics', 'TopicsController')->only([
                         'store', 'update', 'destroy'
                     ]);
+
+                    // 发布回复
+                    Route::post('topics/{topic}/replies', 'RepliesController@store')
+                        ->name('topics.replies.store');
                 });
         });
 
