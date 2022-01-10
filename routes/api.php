d81dcf5516309f281d809767ad4a446a58afb95b
MasterHo1228 <masterho1228@gmail.com>
2022-01-10 15:00:49 +0800

    优化标记已读通知的接口

--- routes/api.php
+++ routes/api.php
@@ -111,7 +111,8 @@ Route::prefix('v1')
                     // 标记消息通知为已读
                     Route::patch('user/read/notifications', 'NotificationsController@read')
                         ->name('user.notifications.read');
-
+                    Route::put('user/read/notifications', 'NotificationsController@read')
+                        ->name('user.notifications.read.put');
 
                     // 当前登录用户权限
                     Route::get('user/permissions', 'PermissionsController@index')
