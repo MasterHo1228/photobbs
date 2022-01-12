f7f1ac76ca52a365054268099241276dffe56d5b
MasterHo1228 <masterho1228@gmail.com>
2022-01-12 10:25:56 +0800

    网站框架-页脚预留展示备案号区域

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,12 @@
       <a href="https://space.bilibili.com/3051298" target="_blank">艺术源于生活</a>
     </p>
 
+    @if (getenv('CN_SITE_RECORD'))
+    <p class="float-left site-record">
+      <a href="http://beian.miit.gov.cn/" target="_blank">{{ getenv('CN_SITE_RECORD') }}</a>
+    </p>
+    @endif
+
     <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
   </div>
 </footer>
